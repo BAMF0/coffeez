bbdde6072f29ce1ab5e21aff95b8f6bb0948b03a
Simon Johnsson <simzter@outlook.com>
2024-02-08 12:53:22 +0100

    Initial commit
    Added basic structure for a coffee bean

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+enum Origin {
+    Brazil,
+    Colombia,
+    ElSalvador,
+    Ethiopia,
+    Kenya,
+    Mexico,
+    Peru,
+    Rwanda,
+    Sumatra,
+}
+
+struct CoffeBean {
+    origin: Origin,
+    name: str,
+}
