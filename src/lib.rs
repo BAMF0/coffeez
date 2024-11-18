bbb90a52ed414b5beee18f26d32c43d7ed055a55
Simon Johnsson <simzter@outlook.com>
2024-11-18 17:45:34 +0100

    Resume development
    Thinking this will be a combined database and logging service for coffee beans.
    Serialize using serde to json schemas.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,33 @@
+// Excessive enumeration, but origins are well defined
 enum Origin {
     Brazil,
     Colombia,
     ElSalvador,
     Ethiopia,
     Kenya,
-    Mexico,
-    Peru,
-    Rwanda,
-    Sumatra,
+}
+
+impl Origin {
+    fn as_str(&self) -> &str  {
+        match self{
+            Self::Brazil => "Brazil",
+            Self::Colombia => "Colombia",
+            Self::ElSalvador => "El Salvador",
+            Self::Ethiopia => "Ethiopia",
+            Self::Kenya => "Kenya",
+        }
+    }
 }
 
 struct CoffeBean {
+    name: String,
     origin: Origin,
-    name: str,
+    roastery: String,
 }
+
+impl CoffeBean {
+    fn new(name: String, origin: Origin, roastery: String) -> Self {
+        CoffeBean { name, origin, roastery }
+    }
+}
+
